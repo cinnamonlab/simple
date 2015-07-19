6efdace569f4fa51f09de53181d62e29d7cea50a
Michael <hungnv038@gmail.com>
2015-07-19 11:33:51 +0000

    Extend Sample

--- app/Controller/SampleController.php
+++ app/Controller/SampleController.php
@@ -3,11 +3,12 @@
 namespace App\Controller;
 use Framework\Config;
 use Framework\Response;
+use Framework\Input;
 
 class SampleController {
     function hello( ) {
         return (new Response())
-            ->setContent(Config::get('app.sample'))
+            ->setContent(Config::get('app.greeting') . ", " . Input::get('name','guest'))
             ->setContentType('text/plain')
             ->setCode(200);
     }
--- config/app.php
+++ config/app.php
@@ -1,5 +1,5 @@
 <?php
 
 return array(
-    'sample' => 'hello'
+    'greeting' => 'hello'
 );
